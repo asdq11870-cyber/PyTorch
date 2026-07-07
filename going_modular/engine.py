4d9b83b68b57418c4c716b61376646ac9e877dd2
asdq11870-cyber <asdq11870@gmail.com>
2026-07-07 12:36:26 +0100

    Update engine.py

--- going_modular/engine.py
+++ going_modular/engine.py
@@ -93,7 +93,7 @@ def batch_train(model:torch.nn.Module,
           break
       
   end = timer()
-  print(f"Total Training Time: {end-start}:.2f seconds")
+  print(f"Total Training Time: {end-start:.2f} seconds")
     
 
   if loss_curves:
