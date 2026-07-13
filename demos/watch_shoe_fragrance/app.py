7e09800f4639a5feec0bfd26e920a3ba232cd45c
asdq11870-cyber <asdq11870@gmail.com>
2026-07-13 20:06:25 +0100

    Update app.py

--- demos/watch_shoe_fragrance/app.py
+++ demos/watch_shoe_fragrance/app.py
@@ -1,5 +1,5 @@
-import gradio as gr
-import torch
+import gradio as gr #pyright: ignore[reportMissingImports]
+import torch #pyright: ignore[reportMissingImports]
 import os
 from model import create_effnetb2_model
 from typing import Tuple, Dict
@@ -36,5 +36,4 @@ demo = gr.Interface(
     outputs=[gr.Label(num_top_classes=3, label="Predictions"), gr.Number(label="Prediction Time (s)")],
     title=title, description=description,examples=example_list
 )
-if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT",7860)))
+demo.launch()
