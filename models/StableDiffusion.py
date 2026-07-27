40986483a233d218f0885414281b383b1f64cdad
asdq11870-cyber <asdq11870@gmail.com>
2026-07-28 00:21:29 +0100

    Update StableDiffusion.py

---
+++ models/StableDiffusion.py
@@ -0,0 +1,51 @@
+import torch
+from torch import nn
+
+class ResNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x:torch.Tensor):
+        pass
+
+class TimestepEmbedding(nn.Module):
+    def __init__(self):
+        super().__init__
+
+    def forward(self, x:torch.Tensor):
+        pass
+
+class SelfAttentionBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x:torch.Tensor):
+        pass
+
+class CrossAttentionBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x:torch.Tensor):
+        pass
+
+class Downsample(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x:torch.Tensor):
+        pass
+
+class Upsample(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x:torch.Tensor):
+        pass
+
+class StableDiffusion(nn.Module):
+    def __init__(self):
+            super().__init__()
+    
+    def forward(self, x:torch.Tensor):
+        pass
